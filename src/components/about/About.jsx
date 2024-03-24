f19166315f43c6c3d2e58f0b7fc02d3a7dce5f2a
gonzalosanto <sancruzgon@gmail.com>
2024-03-23 23:10:37 -0300

    changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,33 +1,11 @@
 const About = () => {
-    return(      <div>
+    return(      
+    <div className="m-8" style={{height:'90vh'}}>
         <h1>About me</h1>  
-        <p className='w-96 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officia eaque, corrupti fugit laboriosam, 
-          sapiente explicabo error qui amet aliquid vitae quasi officiis voluptates modi sequi asperiores, nemo ducimus aut.</p>
-      </div>)
+        <p className='w-96 text-center'>
+            Soy Mapache, un desarrollador Fullstack
+        </p>
+    </div>)
 }
 
 export default About;
